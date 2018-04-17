4e3f9c19da52665537e113408ff9e49ec4e04b83
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-04-17 02:04:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,7 @@ say = " "
 
 def speak_to_grandma(say)
   if "#{say}" == "#{say}".upcase
+    puts "HUH?! SPEAK UP, SONNY!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!

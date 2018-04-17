d161d8c61cd252e6cd413838ed8d94c138c0228a
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-04-17 02:08:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(say)
-  if "#{say}" == "I LOVE YOU GRANDMA!"
+  if say == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif "#{say}" == "#{say}".upcase
     puts "HUH?! SPEAK UP, SONNY!"

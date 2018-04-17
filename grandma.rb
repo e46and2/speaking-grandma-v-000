75b73c68171988a1acaad56e19af30bb2072bf1c
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-04-17 02:15:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 
 def speak_to_grandma(say)
   if "#{say}" == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    puts say = "I LOVE YOU TOO PUMPKIN!"
   elsif "#{say}" == "#{say}".upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts say = "HUH?! SPEAK UP, SONNY!"
   else 
-    puts "NO, NOT SINCE 1938!"
+    puts say = "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
